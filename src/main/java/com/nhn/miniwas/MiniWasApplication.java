2ab4a3eda2eb6b5d2ae68b4b21054cb91bf1c7c6
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-10-01 05:03:48 +0900

    프로젝트 생성

---
+++ src/main/java/com/nhn/miniwas/MiniWasApplication.java
@@ -0,0 +1,9 @@
+package com.nhn.miniwas;
+
+public class MiniWasApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hi I'm mini WAS");
+	}
+
+}
---
+++ src/test/java/com/nhn/miniwas/MiniWasApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.nhn.miniwas;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MiniWasApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
